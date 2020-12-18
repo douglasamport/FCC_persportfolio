da1a4b7e517133f482b53556cb255f069a03cfcb
Douglas <douglas.amport@gmail.com>
2020-12-18 11:09:24 -0500

    added roman numeral converter

--- vanillalibrary.js
+++ vanillalibrary.js
@@ -15,3 +15,23 @@ function splitIntoValues (n) {
     }
     return values;
 }
+
+
+// FreeCodeCamp - JavaScript Algorithms and Data Structures Projects: Roman Numeral Converter
+//Converts Arabic => Roman Numerals accurate up to 3999
+
+const arabic = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+const roman = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'  ]
+var romanCollector = [];
+var arabicReducer;
+
+var convertToRoman = (num) => {
+    while (num > 0) {
+        arabicReducer = arabic.find(y => y <= num);
+        romanCollector.push(roman[arabic.indexOf(arabicReducer)]);
+        num -= arabicReducer;
+    }
+ return romanCollector.join('');
+}
+
+console.log(convertToRoman(3999));
